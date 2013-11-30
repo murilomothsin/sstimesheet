6e83f2ddeeaf8bf8ed3c2091e08ae9fdece111d1
Daniel Zimpel Wayhs <danielwayhs@gmail.com>
2013-11-30 13:26:40 -0200

    Configuring sqlite to be the development database

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131113201846) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "tasks", force: true do |t|
     t.integer "user_id"
     t.integer "year"
@@ -39,7 +36,7 @@ ActiveRecord::Schema.define(version: 20131113201846) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
