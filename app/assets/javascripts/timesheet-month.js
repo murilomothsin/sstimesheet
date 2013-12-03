aefab6b4327ea056527675fcee436ef6a0fb0497
bahartmann <barbarahartma@gmail.com>
2013-12-03 18:22:56 -0200

    JS refactory

--- app/assets/javascripts/timesheet-month.js
+++ app/assets/javascripts/timesheet-month.js
@@ -15,6 +15,22 @@ $(function () {
     }
   });
 
+  var newTask = function(trObj) {
+    var liObj = $('<li><input type="text" value="" /></li>')
+      .appendTo(trObj.find('ul'));
+
+    addInputEvents(liObj.find('input'), trObj);
+  };
+
+  var editTask = function(liObj, trObj) {
+    var taskId = liObj.attr('taskid');
+    var taskDescription = liObj.text();
+    var liContentObj = $('<input type="text" value="' + taskDescription + '" taskid="' + taskId + '" />');
+    liObj.html(liContentObj);
+
+    addInputEvents(liObj.find('input'), trObj);
+  };
+
   var addInputEvents = function(inputObj, trObj) {
     inputObj
       .focus()
@@ -34,72 +50,99 @@ $(function () {
         });
   };
 
-  var newTask = function(trObj) {
-    var liObj = $('<li><input type="text" value="" /></li>')
-      .appendTo(trObj.find('ul'));
-
-      addInputEvents(liObj.find('input'), trObj);
-  };
-
-  var editTask = function(liObj, trObj) {
-    var taskId = liObj.attr('taskid');
-    var taskDescription = liObj.text();
-    var liContentObj = $('<input type="text" value="' + taskDescription + '" taskid="' + taskId + '" />');
-    liObj.html(liContentObj);
-
-    addInputEvents(liObj.find('input'), trObj);
-  };
-
   var saveTask = function(trObj) {
-    // TODO: Super spaguetti code. Break into smaller methods (edit, delete create)
-    var inputObj = $('input[type=text]', trObj);
-    var taskDescription = $.trim(inputObj.val());
-    var containerObj = inputObj.parent();
+    var task = getTaskData(trObj);
+    task.containerObj.addClass('saving');
+    task.containerObj.html(task.task.description);
 
-    containerObj.addClass('saving');
-    if(taskDescription == "") {
-      containerObj.remove();
-    } else {
-      containerObj.html(taskDescription);
-    }
-
-    var data = {
-      task: {
-        year: $('#year').val(),
-        month: $('#month').val(),
-        day: trObj.find('input[type=hidden]').val(),
-        description: taskDescription
-      }
-    };
-
-    var method = 'POST';
-    var url = "/tasks"
-
-    var taskId = inputObj.attr('taskid');
-    if(taskId) {
-      data.task.id = taskId;
-      url = url + "/" + taskId;
-
-      if(taskDescription == "") {
-        method = 'DELETE';
+    if(task.task.id) {
+      if(task.task.description == "") {
+        deleteTask(task);
       } else {
-        method = 'PUT';  
-      }
+        updateTask(task);
+      }     
+    } else {
+      createTask(task);
     }
+  };
 
-    if(taskDescription != "" || taskId) {
-      $.ajax({
+  var createTask = function(task) {
+    if (task.task.description == "") {
+      removeTaskElm(task.containerObj);
+    } else {
+      saveDataTask(
+        task.task,
+        "/tasks",
+        'POST',
+        task.containerObj,
+        function(response) {
+          task.containerObj.attr('taskid', response.id)
+        }
+      );
+    }
+  };
+
+  var updateTask = function(task){
+    saveDataTask(
+      task.task,
+      "/tasks/" + task.task.id,
+      'PUT',
+      task.containerObj
+    );
+  };  
+
+  var deleteTask = function(task){
+    saveDataTask(
+      task.task,
+      "/tasks/" + task.task.id,
+      'DELETE',
+      task.containerObj,
+      function() {
+        removeTaskElm(task.containerObj);
+      }
+    );
+  };
+
+  var saveDataTask = function(task_data, url, method, containerObj, completeCallback) {
+    $.ajax({
         url: url,
         method: method,
-        data: data
+        data: {
+          'task': task_data
+        }
       })
-      .done(function() {
+      .done(function(response) {
         containerObj.removeClass('saving');
+        if(completeCallback) {
+          completeCallback(response);
+        }
       })
       .fail(function() {
         containerObj.removeClass('saving');
         containerObj.addClass('error');
       });
-    }
   };
+
+  var getTaskData = function(tdObj) {
+    var inputObj = $('input[type=text]', tdObj);
+
+    return {
+      'inputObj': inputObj,
+      'containerObj': inputObj.parent(),
+      'task': {
+        'id': inputObj.attr('taskid'),
+        'year': $('#year').val(),
+        'month': $('#month').val(),
+        'day': tdObj.find('input[type=hidden]').val(),
+        'description': $.trim(inputObj.val())
+      }
+    };
+  };
+
+  var removeTaskElm = function(containerObj) {
+    containerObj.fadeOut('slow', function() {
+      containerObj.remove();  
+    });
+  };
+
 });
